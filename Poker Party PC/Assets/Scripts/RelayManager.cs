ccb3cd1da329d4652f9afd2565720fa1fe1dfa0a
szbmrk <szmark16@gmail.com>
2024-05-29 14:04:58 +0200

    create join code - example

---
+++ Poker Party PC/Assets/Scripts/RelayManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Security;
+using System.Threading.Tasks;
+using TMPro;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RelayManager : MonoBehaviour
+{
+    [SerializeField] private Button createJoinCodeButton;
+    [SerializeField] private TextMeshProUGUI joinCodeDisplayText;
+
+    private async void Awake()
+    {
+        await UnityServices.InitializeAsync();
+        await SignInAnonymouslyAsync();
+        joinCodeDisplayText.text = "";
+        createJoinCodeButton.onClick.AddListener(CreateJoinCode);
+    }
+
+    async Task SignInAnonymouslyAsync()
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            Debug.Log("Sign in anonymously succeeded!");
+            Debug.Log($"PlayerID: {AuthenticationService.Instance.PlayerId}");
+
+        }
+        catch (AuthenticationException ex)
+        {
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+
+    private async void CreateJoinCode()
+    {
+        try
+        {
+            var allocation = await RelayService.Instance.CreateAllocationAsync(8);
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+            joinCodeDisplayText.text = $"Join Code: {joinCode}";
+            Debug.Log($"Join Code: {joinCode}");
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.LogError($"Error creating join code: {e.Message}");
+            ShowJoinCodeError("Failed to create join code. Please try again.");
+        }
+    }
+
+    private void ShowJoinCodeError(string errorMessage)
+    {
+        Debug.LogError(errorMessage);
+    }
+}
