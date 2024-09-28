0d8142f71e9b7f612ee8342d50cbc71140896b17
szbmrk <szmark16@gmail.com>
2024-09-28 10:48:51 +0200

    Added card.cs and deck.cs to sharedDLL and added card sprites

---
+++ PokerParty_SharedDLL/Game/Card.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerParty_SharedDLL
+{
+    public class Card
+    {
+        public int value;
+        public string suit;
+
+        public Card(int value, string suit)
+        {
+            this.value = value;
+            this.suit = suit;
+        }
+
+        public string GetValueString()
+        {
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            if (value == 14) return "Ace";
+            return value.ToString();
+        }
+
+        public string GetFileNameForSprite()
+        {
+            return suit + "_" + GetValueString() + ".png";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Value: ").Append(value).Append("\n");
+            sb.Append("Suit: ").Append(suit).Append("\n");
+            return sb.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Card)) return false;
+
+            Card other = obj as Card;
+            if (this.value == other.value && this.suit == other.suit) return true;
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return value.GetHashCode() + suit.GetHashCode();
+        }
+    }
+}
---
+++ PokerParty_SharedDLL/Game/Deck.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerParty_SharedDLL
+{
+    public class Deck
+    {
+        public List<Card> cards;
+
+        public Deck()
+        {
+            cards = new List<Card>();
+            for (int i = 2; i < 15; i++)
+            {
+                cards.Add(new Card(i, "Hearts"));
+                cards.Add(new Card(i, "Diamonds"));
+                cards.Add(new Card(i, "Clubs"));
+                cards.Add(new Card(i, "Spades"));
+            }
+        }
+
+        public void Shuffle()
+        {
+            Random rng = new Random();
+            int n = cards.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                Card value = cards[k];
+                cards[k] = cards[n];
+                cards[n] = value;
+            }
+        }
+
+        public Card Draw()
+        {
+            Card card = cards[0];
+            cards.RemoveAt(0);
+            return card;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Card card in cards)
+            {
+                sb.Append(card.ToString()).Append("\n");
+            }
+            return sb.ToString();
+        }
+    }
+}
