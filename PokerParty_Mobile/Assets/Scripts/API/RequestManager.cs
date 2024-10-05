2c61fc63e22a424d078d9a998879eea63500da71
szbmrk <szmark16@gmail.com>
2024-10-05 16:06:30 +0200

    changed api endpoint

--- PokerParty_Mobile/Assets/Scripts/API/RequestManager.cs
+++ PokerParty_Mobile/Assets/Scripts/API/RequestManager.cs
@@ -7,7 +7,7 @@ using UnityEngine.Networking;
 
 public static class RequestManager
 {
-    private static string baseUrl = "http://104.199.31.81/pokerparty";
+    private static string baseUrl = "https://szobocloud.duckdns.org/pokerparty";
 
     public static async Task<UnityWebRequest> SendPostRequest(string url, string json)
     {
