b50762b5ed1cfffd136227eefdeb8b5bf8e7f37e
Szabó Márk <szmark16@gmail.com>
2024-10-13 18:18:28 +0200

    changed api path to own domain

--- PokerParty_Mobile/Assets/Scripts/API/RequestManager.cs
+++ PokerParty_Mobile/Assets/Scripts/API/RequestManager.cs
@@ -7,7 +7,7 @@ using UnityEngine.Networking;
 
 public static class RequestManager
 {
-    private static string baseUrl = "https://szobocloud.duckdns.org/pokerparty-api";
+    private static string baseUrl = "https://pokerparty.szobo.dev/api";
 
     public static async Task<UnityWebRequest> SendPostRequest(string url, string json)
     {
