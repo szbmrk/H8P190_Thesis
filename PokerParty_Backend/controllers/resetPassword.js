88e4d4ffa55f99a76b3e49bde51175c636b1bc1d
Szabó Márk <szmark16@gmail.com>
2024-10-14 22:20:22 +0200

    updated email text for reset password, and generated URL to reset the password

--- PokerParty_Backend/controllers/resetPassword.js
+++ PokerParty_Backend/controllers/resetPassword.js
@@ -21,11 +21,23 @@ export const sendResetPasswordEmail = async (req, res) => {
             return res.status(400).json({ msg: 'Invalid email or playerName' });
         }
 
+        const playerId = player.rows[0]._id;
         const toEmail = player.rows[0].email;
 
+        const resetPasswordToken = await generateResetPasswordToken();
+
+        const updateResetPasswordTokenQuery = 'UPDATE players SET "resetPasswordToken" = $1 WHERE _id = $2';
+        await db.query(updateResetPasswordTokenQuery, [resetPasswordToken, playerId]);
+
+        const resetUrl = `https://pokerparty.szobo.dev/resetPassword?token=${resetPasswordToken}`;
+
         try {
-            await sendEmail(toEmail, 'Reset Password', 'Click on the link to reset your password');
-            return res.status(200).json({ msg: 'Email sent successfully' });
+            await sendEmail({
+                to: toEmail,
+                subject: 'Password Reset',
+                text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\nPlease click on the following link, or paste this into your browser to complete the process:\n\n${resetUrl}\n\nIf you did not request this, please ignore this email and your password will remain unchanged.\n`
+            });
+            return res.status(200).json({ msg: `Email sent successfully to ${toEmail}` });
         }
         catch (err) {
             console.error(err.message);
@@ -36,4 +48,10 @@ export const sendResetPasswordEmail = async (req, res) => {
         console.error(err.message);
         res.status(500).send('Server error');
     }
+};
+
+const generateResetPasswordToken = async () => {
+    const resetPasswordToken = crypto.randomBytes(20).toString('hex');
+    const hashedToken = crypto.createHash('sha256').update(resetPasswordToken).digest('hex');
+    return hashedToken;
 };
