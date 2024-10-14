53f4ecf4a95ba172762a1192648df080f2b504ba
Szabó Márk <szmark16@gmail.com>
2024-10-14 22:13:40 +0200

    Update sendEmail.js

--- PokerParty_Backend/helper/sendEmail.js
+++ PokerParty_Backend/helper/sendEmail.js
@@ -1,6 +1,10 @@
 import AWS from 'aws-sdk';
 
-AWS.config.update({ region: process.env.AWS_REGION });
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+});
 
 const ses = new AWS.SES();
 
