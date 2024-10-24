8e0fefc4cd5817bfd2e76addde8e51294c681523
szobo <szobo@szobocloud.vcn10051919.oraclevcn.com>
2024-10-24 10:00:26 +0000

    updated title for pw reset webpage

--- PokerParty_Web/passwordReset/index.php
+++ PokerParty_Web/passwordReset/index.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && $token) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Password Reset - PokerParty</title>
+    <title>PokerParty | Password Reset</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 
