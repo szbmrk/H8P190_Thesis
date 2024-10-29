9bee71dcc14185cc9ed1062eb393a64cec24facd
Szabó Márk <szmark16@gmail.com>
2024-10-29 12:09:45 +0100

    init eval test console app and modified gitignore

---
+++ PokerParty_EvalTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using PokerParty_SharedDLL;
+
+namespace PokerParty_EvalTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
