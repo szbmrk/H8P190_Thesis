af05215d25296ca96b3aca4ecb28673dfb6daa51
szbmrk <szmark16@gmail.com>
2024-11-09 12:06:27 +0100

    Implemented Card's and Deck's class Testing

---
+++ PokerParty_SharedDLL.Tests/Game/CardTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerParty_SharedDLL.Tests
+{
+    public class CardTests
+    {
+        [Fact]
+        public void GetValueString_WhenFaceCard_ShouldReturnCorrectString()
+        {
+            Assert.Equal("Jack", new Card(11, "Hearts").GetValueString());
+            Assert.Equal("Queen", new Card(12, "Diamonds").GetValueString());
+            Assert.Equal("King", new Card(13, "Clubs").GetValueString());
+            Assert.Equal("A", new Card(14, "Spades").GetValueString());
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(10)]
+        public void GetValueString_WhenNumberCard_ShouldReturnCorrectString(int value)
+        {
+            Card card = new Card(value, "Spades");
+            Assert.Equal(value.ToString(), card.GetValueString());
+        }
+
+        [Fact]
+        public void GetFileNameForSprite_WhenCalled_ShouldReturnCorrectFileName()
+        {
+            Card card = new Card(11, "Hearts");
+            Assert.Equal("Hearts_Jack", card.GetFileNameForSprite());
+        }
+
+        [Fact]
+        public void Equals_WhenSameCard_ShouldReturnTrue()
+        {
+            Card card1 = new Card(10, "Hearts");
+            Card card2 = new Card(10, "Hearts");
+            Assert.True(card1.Equals(card2));
+        }
+
+        [Fact]
+        public void Equals_WhenDifferentCard_ShouldReturnFalse()
+        {
+            Card card1 = new Card(10, "Hearts");
+            Card card2 = new Card(11, "Hearts");
+            Assert.False(card1.Equals(card2));
+        }
+
+        [Fact]
+        public void GetHashCode_WhenSameCard_ShouldReturnSameHashCode()
+        {
+            Card card1 = new Card(9, "Spades");
+            Card card2 = new Card(9, "Spades");
+            Assert.Equal(card1.GetHashCode(), card2.GetHashCode());
+        }
+
+        [Fact]
+        public void GetHashCode_WhenDifferentCards_ShouldReturnDifferentHashCodes()
+        {
+            Card card1 = new Card(7, "Clubs");
+            Card card2 = new Card(8, "Clubs");
+            Assert.NotEqual(card1.GetHashCode(), card2.GetHashCode());
+        }
+    }
+}
---
+++ PokerParty_SharedDLL.Tests/Game/DeckTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerParty_SharedDLL.Tests
+{
+    public class DeckTests
+    {
+        [Fact]
+        public void Constructor_WhenCalled_ShouldContain52Cards()
+        {
+            Deck deck = new Deck();
+            int cardCount = deck.cards.Count;
+            Assert.Equal(52, cardCount);
+        }
+
+        [Fact]
+        public void Shuffle_WhenCalled_ShouldReorderCardsRandomly()
+        {
+            Deck deck1 = new Deck();
+            Deck deck2 = new Deck();
+            deck1.Shuffle();
+            deck2.Shuffle();
+            Card[] deck1Array = deck1.cards.ToArray();
+            Card[] deck2Array = deck2.cards.ToArray();
+            Assert.NotEqual(deck1Array, deck2Array);
+        }
+
+        [Fact]
+        public void Draw_WhenCalled_ShouldReturnCardAndDecreaseDeckSize()
+        {
+            Deck deck = new Deck();
+            int initialCount = deck.cards.Count;
+            Card drawnCard = deck.Draw();
+            int finalCount = deck.cards.Count;
+            Assert.NotNull(drawnCard);
+            Assert.Equal(initialCount - 1, finalCount);
+        }
+
+        [Fact]
+        public void Draw_WhenDeckEmpty_ShouldThrowInvalidOperationException()
+        {
+            Deck deck = new Deck();
+            while (deck.cards.Count > 0)
+            {
+                deck.Draw();
+            }
+            Assert.Throws<InvalidOperationException>(() => deck.Draw());
+        }
+    }
+}
