5ddb4ffb5fec30025555f4a41df7c74b86e22190
Szabó Márk <szmark16@gmail.com>
2025-01-05 21:11:38 +0100

    remove backend and webPage

--- PokerParty_Backend/helper/passwordHash.js
+++
@@ -1,7 +0,0 @@
-import bcrypt from 'bcrypt';
-
-export const hashPassword = async (password) => {
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    return hashedPassword;
-};
