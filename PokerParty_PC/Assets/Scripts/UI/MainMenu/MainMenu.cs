e65442370518f3a9656fda7cbdadc6417543bb40
Szabó Márk <szmark16@gmail.com>
2025-01-25 17:16:36 +0100

    Added placeholder for settings button

--- PokerParty_PC/Assets/Scripts/UI/MainMenu/MainMenu.cs
+++ PokerParty_PC/Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -6,20 +6,27 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] private Button createGameBtn;
     [SerializeField] private Button quitGameBtn;
+    [SerializeField] private Button settingsBtn;
 
     private void Awake()
     {
         createGameBtn.onClick.AddListener(CreateGame);
         quitGameBtn.onClick.AddListener(QuitGame);
+        settingsBtn.onClick.AddListener(OpenSettings);
     }
 
     private void CreateGame()
     {
         SceneManager.LoadScene("Lobby");
     }
-
+    
     private void QuitGame()
     {
         Application.Quit();
+    }
+
+    private void OpenSettings()
+    {
+        //TODO: Implement settings menu
     }
 }
