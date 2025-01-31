7f072e0ded77dcf1ac5694d19522c4f68847b513
szbmrk <szmark16@gmail.com>
2025-01-31 13:51:38 +0100

    Phones never sleep while in the game

---
+++ PokerParty_Mobile/Assets/Scripts/Game/ScreenSleepOptions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class ScreenSleepOptions : MonoBehaviour
+{
+    private void Awake()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+}
---
+++ PokerParty_Mobile/Assets/Scripts/UI/PauseMenu/PauseMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static PauseMenu instance;
+
+    [SerializeField] private GameObject pausePanel;
+    
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+    
+    public void Pause()
+    {
+        pausePanel.SetActive(true);
+    }
+    
+    public void Resume()
+    {
+        pausePanel.SetActive(false);
+    }
+}
