522add2b74b230f8007aad6301aa944380b2efac
szbmrk <szmark16@gmail.com>
2025-03-09 19:56:54 +0100

    lkjk

---
+++ PokerParty_LaTeX/NetworkMessageType.cs
@@ -0,0 +1,25 @@
+﻿namespace PokerParty_SharedDLL
+{
+    public enum NetworkMessageType
+    {
+        ChatMessage,
+        ConnectionMessage,
+        GamePausedMessage,
+        GameUnpausedMessage,
+        ReadyMessage,
+        TurnDoneMessage,
+        LobbyIsFullMessage,
+        PlayerNameAlreadyInUseMessage,
+        GameAlreadyStartedMessage,
+        LoadedToGameMessage,
+        RefreshedMoneyMessage,
+        GameStartedMessage,
+        YourTurnMessage,
+        NotYourTurnMessage,
+        NewTurnStartedMessage,
+        DealCardsMessage,
+        CommunityCardsChangedMessage,
+        GameOverMessage,
+        GameInfoMessage
+    }
+}
